48f4ef07b0bcd70b3cea24568c8e350e07da2c24
Jozef Cajkovic <jozef.cajkovic@gmail.com>
2023-03-22 16:54:40 +0100

    fixup! feat(go): PIXEL test chatGPT version

--- influxdbv2/data_ready.go
+++ influxdbv2/data_ready.go
@@ -24,16 +24,26 @@ func DataReady() *schema.Resource {
 
 func DataGetReady(d *schema.ResourceData, meta interface{}) error {
 	influx := meta.(influxdb2.Client)
-	response_ready, err := influx.Ready(context.Background())
+	// response_ready, err := influx.Ready(context.Background())
+	// if err != nil {
+	// 	return fmt.Errorf("server is not ready: %v", err)
+	// }
+	// var ready bool = false
+	// // if response_ready.Status == domain.ReadyStatusReady {
+	// var ready_temp domain.ReadyStatus = "ready"
+	// if response_ready.Status == *ready_temp {
+	// 	ready = true
+	// }
+
+	responseReady, err := influx.Ready(context.Background())
 	if err != nil {
 		return fmt.Errorf("server is not ready: %v", err)
 	}
-	var ready bool = false
-	// if response_ready.Status == domain.ReadyStatusReady {
-	var ready_temp *domain.ReadyStatus = "ready"
-	if response_ready.Status == ready_temp {
+	var ready bool
+	if *responseReady.Status == domain.ReadyStatusReady {
 		ready = true
 	}
+
 	if ready {
 		log.Printf("Server is ready !")
 	}
